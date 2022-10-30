7d2be33116346092f4a171834a30dc4c16bceeee
Jordan Wolter <jordan.wolter@icloud.com>
2022-10-30 18:00:35 -0500

    post works

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -19,13 +19,19 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    console.log('POST /api/feedback');
-
-    console.log('req.body', req.body);
+    console.log('POST /api/feedback', req.body);
 
     pool.query(`INSERT INTO "feedback" 
     ("feeling", "understanding", "support", "comments")
-    VALUES ($1, $2, $3, $4);`);
+    VALUES ($1, $2, $3, $4);`, [req.body.feeling, req.body.understanding, req.body.support, req.body.comments])
+    .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error adding feedback`, error);
+        res.sendStatus(500);
+      });
 
 })
+
 module.exports = router;
